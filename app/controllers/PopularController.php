cbe89164bcecd1e6927c1757f2755d3979000fd6
Artem Zinoviev <arzinoviev@gmail.com>
2014-06-18 18:49:38 +0300

    add php-instagram-api

--- app/controllers/PopularController.php
+++ app/controllers/PopularController.php
@@ -4,6 +4,7 @@
 class PopularController extends BaseController {
 
     const CLIENT_ID = 'cc217dda7dda45178e1a83594eb7db8e';
+//    const OAUTH_URL = 'https://instagram.com/oauth/authorize/?client_id=cc217dda7dda45178e1a83594eb7db8e&redirect_uri=http://photoplanet.dev:8000/&response_type=code';
 
     public function show()
     {
