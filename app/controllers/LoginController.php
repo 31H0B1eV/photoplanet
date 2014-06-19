1b3af0f9711fed50eeca7dfa66e0a1766b69e653
Artem Zinoviev <arzinoviev@gmail.com>
2014-06-19 16:24:36 +0300

    add helper interface for instagram credentials

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,12 +1,7 @@
 <?php
 
 
-class LoginController extends BaseController {
-
-    const CLIENT_ID = 'cc217dda7dda45178e1a83594eb7db8e';
-    const CLIENT_SECRET = '8458182fea814756bfad6e77a4bd26a5';
-    const REDIRECT_URI = 'http://photoplanet.dev:8000/';
-//    const OAUTH_URL = 'https://instagram.com/oauth/authorize/?client_id=cc217dda7dda45178e1a83594eb7db8e&redirect_uri=http://photoplanet.dev:8000/&response_type=code';
+class LoginController extends BaseController implements Helper {
 
     public $auth_config = array(
         'client_id'     => self::CLIENT_ID,
--- app/controllers/PopularController.php
+++ app/controllers/PopularController.php
@@ -1,16 +1,14 @@
 <?php
 
 
-class PopularController extends BaseController {
-
-    const CLIENT_ID = 'cc217dda7dda45178e1a83594eb7db8e';
+class PopularController extends BaseController implements Helper {
 
     public function show()
     {
 
         $client = new Guzzle\Service\Client('https://api.instagram.com/');
 
-        $response = $client->get("v1/media/popular?client_id=".$this::CLIENT_ID)->send();
+        $response = $client->get("v1/media/popular?client_id=".self::CLIENT_ID)->send();
 
         $result = $response->json();
 
