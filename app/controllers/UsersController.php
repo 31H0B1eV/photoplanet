bf3d7b8a410e934b29ae5e4d5be8fb8ca63ac062
Artem Zinoviev <arzinoviev@gmail.com>
2014-06-20 17:42:06 +0300

    fix

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -5,22 +5,22 @@ class UsersController extends BaseController {
 
     public function getLogin()
     {
-        if (Session::has(Config::get('instagram::session_name')))
-            Session::forget(Config::get('instagram::session_name'));
+        if (Session::has('instagram_access_token'))
+            Session::forget('instagram_access_token');
 
         Instagram::authorize();
     }
 
     public function getAuthorize()
     {
-        Session::put(Config::get('instagram::session_name'), Instagram::getAccessToken(Input::get('code')));
+        Session::put('instagram_access_token', Instagram::getAccessToken(Input::get('code')));
 
         return Redirect::to('/');
     }
 
     public function getLogout()
     {
-        Session::forget(Config::get('instagram::session_name'));
+        Session::forget('instagram_access_token');
 
         return Redirect::to('/');
     }
